9643b0129f30851bd574e438d20ff0ea195e27e0
Glas42 <145870870+OleFranz@users.noreply.github.com>
2025-03-28 15:50:58 +0100

    fix get-dataset.py not downloading missing files

--- train/get-dataset.py
+++ train/get-dataset.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 Path = os.path.dirname(__file__).replace("\\", "/")
 if Path[-1] != "/":
@@ -6,10 +7,15 @@ if Path[-1] != "/":
 DatasetPath = f"{Path}dataset"
 DatasetCache = f"{Path}dataset-cache"
 
+if os.path.exists(DatasetCache):
+    try:
+        shutil.rmtree(DatasetCache)
+    except:
+        pass
+
 os.environ['HF_HOME'] = DatasetPath
 os.makedirs(DatasetPath, exist_ok=True)
 os.makedirs(DatasetCache, exist_ok=True)
-
 
 for i in range(1000):
     Success = False
